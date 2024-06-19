50d4e7f35a934360d1f2079d56cc136d63951ef1
nmahendran-aws <mahendran.natarajan.aws@gmail.com>
2024-06-19 14:07:52 -0400

    updated folder structure

--- modules/vpc/vpc-module.tf
+++ modules/vpc/vpc-module.tf
@@ -5,7 +5,7 @@ module "vpc" {
   version = "5.4.0"    
 
   # VPC Basic Details
-  name = "${local.name}-${var.vpc_name}"
+  name = "${var.vpc_name}"
   cidr = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
   public_subnets  = var.vpc_public_subnets
@@ -26,9 +26,6 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-
-  tags = local.common_tags
-  vpc_tags = local.common_tags
 
   # Additional Tags to Subnets
   public_subnet_tags = {
