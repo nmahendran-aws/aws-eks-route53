13f9f4ecab53114be823fcf768f08ee5bb1101ed
nmahendran-aws <mahendran.natarajan.aws@gmail.com>
2024-06-21 12:02:38 -0400

    vairables

--- ec2/varaibles.tf
+++ ec2/varaibles.tf
@@ -11,3 +11,16 @@ variable "instance_keypair" {
   type = string
   default = "eks-terraform-key"
 }
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "SAP"
+}
