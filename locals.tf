0ffbf688500893b1f47f33699fe0956d966ebd6e
nmahendran-aws <mahendran.natarajan.aws@gmail.com>
2024-06-22 14:56:26 -0400

    updated tags

--- locals.tf
+++ locals.tf
@@ -3,6 +3,8 @@ locals {
   owners = var.business_divsion
   environment = var.environment
   name = "${var.business_divsion}-${var.environment}"
+  # Add additional local value
+  eks_cluster_name = "${local.name}-${var.cluster_name}"  
   #name = "${local.owners}-${local.environment}"
   common_tags = {
     owners = local.owners
--- vpc-module.tf
+++ vpc-module.tf
@@ -5,7 +5,7 @@ module "vpc" {
   version = "5.4.0"    
 
   # VPC Basic Details
-  name = "${var.vpc_name}"
+  name = local.eks_cluster_name
   cidr = var.vpc_cidr_block
   azs             = data.aws_availability_zones.available.names
   public_subnets  = var.vpc_public_subnets
@@ -22,18 +22,26 @@ module "vpc" {
   enable_nat_gateway = var.vpc_enable_nat_gateway 
   single_nat_gateway = var.vpc_single_nat_gateway
 
+  tags = local.common_tags
+  vpc_tags = local.common_tags
+
   # VPC DNS Parameters
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-
   # Additional Tags to Subnets
   public_subnet_tags = {
     Type = "Public Subnets"
+    "kubernetes.io/role/elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"     
   }
+
   private_subnet_tags = {
-    Type = "Private Subnets"
+    Type = "private-subnets"
+    "kubernetes.io/role/internal-elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"  
   }  
+  
   database_subnet_tags = {
     Type = "Private Database Subnets"
   }
