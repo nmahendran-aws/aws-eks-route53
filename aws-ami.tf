30b02c7d872388b86fc1ce05fdf35ebc3277b3ea
nmahendran-aws <mahendran.natarajan.aws@gmail.com>
2024-06-21 21:53:26 -0400

    ami

--- aws-ami.tf
+++ aws-ami.tf
@@ -4,7 +4,7 @@ data "aws_ami" "amzlinux2" {
   owners = [ "amazon" ]
   filter {
     name = "name"
-    values = [ "ami-08a0d1e16fc3f61ea" ]
+    values = [ "amzn2-ami-hvm-*-gp2" ]
   }
   filter {
     name = "root-device-type"
