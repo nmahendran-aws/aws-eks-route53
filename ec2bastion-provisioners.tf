476223f937986ae667ab899b59ef0d05498a5236
nmahendran-aws <mahendran.natarajan.aws@gmail.com>
2024-06-21 22:22:13 -0400

    removed loc provisioner

--- ec2bastion-provisioners.tf
+++ ec2bastion-provisioners.tf
@@ -22,10 +22,10 @@ resource "null_resource" "copy_ec2_keys" {
     ]
   }
 ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
-  provisioner "local-exec" {
-    command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
-    #on_failure = continue
-  }
+  # provisioner "local-exec" {
+  #   command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+  #   working_dir = "local-exec-output-files/"
+  #   #on_failure = continue
+  # }
 
 }
